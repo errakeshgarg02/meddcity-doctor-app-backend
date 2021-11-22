4fbeec29fc306daabfc95e9fa5b41363865878c7
rakesh <er.rakeshgarg02@gmail.com>
2021-11-22 15:56:27 +0530

    minor fix

--- common/src/main/java/com/arg/common/config/BeanConfig.java
+++ common/src/main/java/com/arg/common/config/BeanConfig.java
@@ -2,8 +2,10 @@ package com.arg.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.client.RestTemplate;
 
+@EnableJpaAuditing
 @Configuration
 public class BeanConfig {
 	
